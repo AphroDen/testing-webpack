99da6e29efa80e3d5717746aab6792e89f38d208
den <warfolomeowmeow@gmail.com>
2018-05-21 02:29:52 +0300

    Agency. header + contact-us = done

--- src/agency/agency.js
+++ src/agency/agency.js
@@ -1,21 +1,2 @@
 import './agency.scss';
 
-let acc = document.getElementsByClassName("accordion");
-let i;
-
-for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function () {
-        this.classList.toggle("active");
-        let panel = this.nextElementSibling;
-        if (panel.style.display === "block") {
-            panel.style.display = "none";
-        } else {
-            panel.style.display = "block";
-        }
-    });
-}
-
-
-$("#wrapper").click( function() {
-    $(".menu").toggleClass("close");
-});
