8a37fbba831baf5afa08d0cb17d0fcd3b6c5f3cf
den <warfolomeowmeow@gmail.com>
2018-06-07 18:54:50 +0300

    waxom. presentation -> mob-devices -> counters

--- src/waxom/waxom.js
+++ src/waxom/waxom.js
@@ -1,1 +1,25 @@
 import './waxom.scss';
+
+let test = $('div.test');
+for (let i = 0; i < test.length; i++)
+    test.get(i).style.color = "red";
+
+setTimeout("$('p.test').get(0).style.color = 'green'", 2000);
+
+
+// $(".kek").append("<span> JUST TEXT</span>");
+// $(".lolkek").appendTo($(".kek"));
+
+// $(".topkek").appendTo($(".v2"));
+
+$(".v2").css("border-bottom", "5px solid red");
+$(".topkek").css("border-bottom", "5px solid red");
+
+
+$(function () {
+    $('.wrapper').hover(function () {
+        $(this).children().stop().animate({marginTop: '100%', opacity: 0}, 100);
+    }, function () {
+        $(this).children().stop().animate({marginTop: '50%', opacity: 1}, 100);
+    });
+});
